0e2cee5cf45e18f44c4d6fac13dc6e17326fb06e
Nkemjika Obi <nkemjikaobi@gmail.com>
2026-09-20 12:43:13 -0400

    Add: max-water-container - submission-2

---
+++ Data Structures & Algorithms/max-water-container/submission-2.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {number[]} heights
+     * @return {number}
+     */
+    maxArea(heights) {
+        let maxArea = 0;
+        let left = 0;
+        let right = heights.length - 1;
+
+        while (left < right) {
+            const containerSize = (right - left) * Math.min(heights[left], heights[right]);
+            maxArea = Math.max(maxArea, containerSize)
+
+            if(heights[left] < heights[right]) left++;
+            else if (heights[left] > heights[right]) right--;
+            else {
+                left++;
+                right--;
+            }
+        }
+        return maxArea
+    }
+}
