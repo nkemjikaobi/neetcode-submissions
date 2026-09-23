60e545e1c7f68c49a299ddafea1c33941f0b3085
Nkemjika Obi <nkemjikaobi@gmail.com>
2026-09-22 20:26:34 -0400

    Add: longest-repeating-substring-with-replacement - submission-11

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-11.js
@@ -0,0 +1,36 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {number} k
+     * @return {number}
+     */
+    characterReplacement(s, k) {
+        let left = 0;
+        let right = 0;
+        let map = new Map();
+        let longestLength = 0;
+        let highestOccuringLetter = 0;
+
+        while (right < s.length) {
+            const rightChar = s[right];
+            map.set(rightChar, (map.get(rightChar) ?? 0) + 1); // update the map
+            highestOccuringLetter = Math.max(highestOccuringLetter, map.get(rightChar));
+
+            // const highestOccuringLetter = Math.max(...map.values())
+            const lengthOfSlidingWindow = right - left + 1;
+
+            // ensure we have room to replace characters
+            if (lengthOfSlidingWindow - highestOccuringLetter <= k) {
+                longestLength = Math.max(longestLength, lengthOfSlidingWindow);
+            } else {
+                const leftChar = s[left];
+                map.set(leftChar, map.get(leftChar) - 1);
+                left++;
+            }
+
+            right++;
+        }
+
+        return longestLength;
+    }
+}
